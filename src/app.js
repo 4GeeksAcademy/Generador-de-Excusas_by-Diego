891c2aed672fcd321400c7e15ee2d51f525b5ccb
Diego-Tello <diegotellomendoza@gmail.com>
2026-01-11 20:52:53 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  const btnGenerater = document.querySelector('.btn_generador');
+
+  btnGenerater.addEventListener("click", () => {
+    function getWord(arr) {
+      return arr[Math.floor(Math.random() * arr.length)]
+    }
+
+    let randomWho = getWord(who);
+    let randomAction = getWord(action);
+    let randomWhat = getWord(what);
+    let randomWhen = getWord(when);
+
+    let ExcusaGenerada = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+    document.querySelector('.excuse').innerHTML = ExcusaGenerada;
+
+  })
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+
+}
